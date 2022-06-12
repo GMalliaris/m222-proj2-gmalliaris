e0ff81874041b1d84b1c015b50fd7554dea898f2
GMalliaris <georgemalliaris8@gmail.com>
2022-06-12 13:53:04 +0300

    initial commit

---
+++ src/main/java/org/gmalliaris/m222proj2gmalliaris/M222Proj2GmalliarisApplication.java
@@ -0,0 +1,13 @@
+package org.gmalliaris.m222proj2gmalliaris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M222Proj2GmalliarisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M222Proj2GmalliarisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/gmalliaris/m222proj2gmalliaris/M222Proj2GmalliarisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gmalliaris.m222proj2gmalliaris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M222Proj2GmalliarisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

3191c3c3980113cf5bff962e354c461417dce290
GMalliaris <georgemalliaris8@gmail.com>
2022-06-22 21:11:21 +0300

    added @NotBlank at DateWrappers

--- src/main/java/org/gmalliaris/m222proj2gmalliaris/model/DateTimeRange.java
+++ src/main/java/org/gmalliaris/m222proj2gmalliaris/model/DateTimeRange.java
@@ -3,11 +3,15 @@ package org.gmalliaris.m222proj2gmalliaris.model;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import org.gmalliaris.m222proj2gmalliaris.validation.ValidDateTime;
 
+import javax.validation.constraints.NotBlank;
+
 public class DateTimeRange {
 
     @ValidDateTime(message = "Invalid date in format yyyy-MM-dd hh:mm:ss")
+    @NotBlank
     private final String rangeStart;
     @ValidDateTime(message = "Invalid date in format yyyy-MM-dd hh:mm:ss")
+    @NotBlank
     private final String rangeEnd;
 
     @JsonCreator
--- src/main/java/org/gmalliaris/m222proj2gmalliaris/model/SpecificDate.java
+++ src/main/java/org/gmalliaris/m222proj2gmalliaris/model/SpecificDate.java
@@ -3,9 +3,12 @@ package org.gmalliaris.m222proj2gmalliaris.model;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import org.gmalliaris.m222proj2gmalliaris.validation.ValidDate;
 
+import javax.validation.constraints.NotBlank;
+
 public class SpecificDate {
 
     @ValidDate
+    @NotBlank
     private final String date;
 
     @JsonCreator
